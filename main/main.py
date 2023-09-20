ace1fa798bc10b13adcfbe2939d7a6bb9fe93f3f
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-20 05:01:49 -0500

    updated working migration sqlalchemy

--- main/main.py
+++ main/main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import UniqueConstraint
+from flask_migrate import Migrate
 
 
 app=Flask(__name__)
@@ -10,28 +11,19 @@ app.config["SQLALCHEMY_DATABASE_URI"] = \
 CORS(app)
 
 db=SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# class Product(db.Model):
-#     '''
-#     now we have to make another change for the id here we will add here.
-#     auto increment to false why did i add this field because the product will 
-#     not be created in this app the product will be created in 
-#     the django app and this app will just catch the event from rabbitmq
-#     and it will create the product and when we create the product we don't
-#     want the id to be auto increment because the id will be different than the the django app
-#     so if we want the same id we have to put it onto incremental false and to insert
-#     directly the id as it is from the django app
-#     '''
-#     id=db.Column(db.Integer, primary_key=True, autoincrement=False)
-#     title=db.Column(db.String(200))
-#     image=db.Column(db.String(200))
+class Product(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(200))
+    image=db.Column(db.String(200))
 
-# class ProductUser(db.Model):
-#     id=db.Column(db.Integer, primary_key=True)
-#     user_id=db.Column(db.Integer)
-#     product_id=db.Column(db.Integer)
+class ProductUser(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    user_id=db.Column(db.Integer)
+    product_id=db.Column(db.Integer)
 
-#     UniqueConstraint('user_id', 'product_id', name='user_product_unique')
+    UniqueConstraint('user_id', 'product_id', name='user_product_unique')
 
 @app.route('/')
 def index():
