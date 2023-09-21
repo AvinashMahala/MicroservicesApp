3caecb154337b468b840888fef1d09193fe3f0a2
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-20 21:40:25 -0500

    DJANGO PRODUCER AND CONSUMER

---
+++ admin/products/producer.py
@@ -0,0 +1,13 @@
+import pika
+
+params = pika.URLParameters('amqps://wbwqtgwy:E-OlzLdn3tnj1YgzH2xlV7kDhRAI4bQQ@shrimp.rmq.cloudamqp.com/wbwqtgwy')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()  # start a channel
+
+# channel.queue_declare(queue='producer')  # Declare a queue
+
+def publish():
+    channel.basic_publish(exchange='', routing_key='admin', body='Hello Avinash!')
+
