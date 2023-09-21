555972f6e490c9a30768e7d95be383d585ff39cc
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-21 00:06:16 -0500

    Data Consistency in Both Flask And Django App
    If Data Is created in Flask App, The same data in reflected in Djano App as Well.

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -1,4 +1,4 @@
-import pika
+import pika, json
 
 params = pika.URLParameters('amqps://wbwqtgwy:E-OlzLdn3tnj1YgzH2xlV7kDhRAI4bQQ@shrimp.rmq.cloudamqp.com/wbwqtgwy')
 
@@ -6,8 +6,11 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()  # start a channel
 
-# channel.queue_declare(queue='producer')  # Declare a queue
 
-def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='Hello Main!')
+# def publish():
+#     channel.basic_publish(exchange='', routing_key='main', body='Hello Main!')
 
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
