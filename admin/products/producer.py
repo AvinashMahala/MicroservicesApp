5dc2fda50f1e00e6bd84a7cdc8dc2eaa8129bcba
Avinash Mahala <avinash.mahala.am@outlook.com>
2023-09-20 22:04:54 -0500

    QUEUE SERVICE Using Docker

--- admin/products/producer.py
+++ admin/products/producer.py
@@ -9,5 +9,5 @@ channel = connection.channel()  # start a channel
 # channel.queue_declare(queue='producer')  # Declare a queue
 
 def publish():
-    channel.basic_publish(exchange='', routing_key='main', body='Hello Avinash!')
+    channel.basic_publish(exchange='', routing_key='main', body='Hello Main!')
 
